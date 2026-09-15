d8d0a7ec957c16a6f04bd053b44ede49790d89f2
akinvestglobaltradex786818-creator <ak.investglobaltradex786.818@gmail.com>
2026-09-15 17:58:47 +0500

    Update Validate.ts

--- src/lib/Validate.ts
+++ src/lib/Validate.ts
@@ -1,13 +1,9 @@
 export function validateBlueprint(data: any) {
   return {
     app_name: data?.app_name || "Untitled App",
-
     entities: Array.isArray(data?.entities) ? data.entities : [],
-
     apis: Array.isArray(data?.apis) ? data.apis : [],
-
     pages: Array.isArray(data?.pages) ? data.pages : [],
-
     user_flows: Array.isArray(data?.user_flows) ? data.user_flows : []
   };
 }
