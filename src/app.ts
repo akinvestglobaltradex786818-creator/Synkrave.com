716418d3025e3ff180d9bb225bdf0d4452c67dfe
akinvestglobaltradex786818-creator <ak.investglobaltradex786.818@gmail.com>
2026-09-17 14:04:31 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import cors from "cors";
+import * as cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors.default()); // ⚠️ یہ important ہے
 
+app.use(express.json());
 app.use("/api", routes);
 
 export default app;
